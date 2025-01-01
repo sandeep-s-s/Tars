520e4dd3665b534a1180f43522779e6b2be73027
sandeep sreekumar <sandeepsreekumar75@gmail.com>
2025-01-01 22:18:50 +0530

    [Feature] Tauri Menu bar added

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,9 @@
+use schema::collections::table;
+use tauri::menu::{AboutMetadata, Menu, MenuItem, Submenu, SubmenuBuilder};
+use tauri::menu::{MenuBuilder, MenuItemBuilder};
+use tauri::tray::TrayIconBuilder;
+use tauri::{Emitter, Manager};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 mod cmd;
 mod db;
@@ -11,6 +17,70 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            // let handle = app.handle();
+
+            // my custom settings menu item
+            let settings = MenuItemBuilder::new("Settings...")
+                .id("settings")
+                .accelerator("CmdOrCtrl+,")
+                .build(app)?;
+
+            // my custom app submenu
+            let app_submenu = SubmenuBuilder::new(app, "App")
+                .about(Some(AboutMetadata {
+                    ..Default::default()
+                }))
+                .separator()
+                .item(&settings)
+                .separator()
+                .services()
+                .separator()
+                .hide()
+                .hide_others()
+                .quit()
+                .build()?;
+            let quit = MenuItemBuilder::new("Quit").id("quit").build(app).unwrap();
+
+            let hide = MenuItemBuilder::new("Hide").id("hide").build(app).unwrap();
+            let show = MenuItemBuilder::new("Show").id("show").build(app).unwrap();
+
+            // ... any other submenus
+
+            let menu = MenuBuilder::new(app)
+                .items(&[
+                    &app_submenu,
+                    &quit,
+                    &hide,
+                    &show, // ... include references to any other submenus
+                ])
+                .build()?;
+
+            // set the menu
+            app.set_menu(menu)?;
+
+            // listen for menu item click events
+            app.on_menu_event(move |app, event| match event.id().as_ref() {
+                // if event.id() == settings.id() {
+                //     // emit a window event to the frontend
+                //     let _event = app.emit("custom-event", "/about");
+                // }
+                "quit" => app.exit(0),
+                "hide" => {
+                    dbg!("menu item hide clicked");
+                    let window = app.get_webview_window("main").unwrap();
+                    window.hide().unwrap();
+                }
+                "show" => {
+                    dbg!("menu item show clicked");
+                    let window = app.get_webview_window("main").unwrap();
+                    window.show().unwrap();
+                }
+                _ => {}
+            });
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             cmd::create_collection,
             cmd::get_collections,
